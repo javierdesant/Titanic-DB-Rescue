4b59d86cfdfb197fefae16ec060fb115d5566b97
gomago43 <167336499+gomago43@users.noreply.github.com>
2024-12-11 19:39:59 +0100

    feat: add listaPasajerosCabina()

--- src/main/java/bbdd/Main.java
+++ src/main/java/bbdd/Main.java
@@ -16,6 +16,7 @@ public class Main {
     private final static String DB_PASS = "titanic";
     private static Connection conn;
     private static PreparedStatement nuevoPlstmt;
+    private static PreparedStatement listaPCstmt;
 
     public static void main (String [] args) throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
@@ -23,14 +24,18 @@ public class Main {
         String url = "jdbc:mysql://" + DB_SERVER + ":" + DB_PORT + "/" + DB_NAME;
         conn = DriverManager.getConnection(url, DB_USER, DB_PASS);
     
-        nuevoPlstmt = conn.prepareStatement("INSERT INTO planetas (nombre, masa, radio, sistema) VALUES (?, ?, ?, ?)");
-        
         // 1. Añade los planetas a la base de datos
-        nuevoPlaneta("Kepler-186f", 3.3e24, 8800 , "Copernico");
-        nuevoPlaneta("HD 209458 b (Osiris)", 1.4e27, 100000, "Beta Pictoris");
-        nuevoPlaneta("LHS 1140 b", 8.3e24, 8800, "Copernico");
+        // nuevoPlstmt = conn.prepareStatement("INSERT INTO planetas (nombre, masa, radio, sistema) VALUES (?, ?, ?, ?)");
+
+        // nuevoPlaneta("Kepler-186f", 3.3e24, 8800 , "Copernico");
+        // nuevoPlaneta("HD 209458 b (Osiris)", 1.4e27, 100000, "Beta Pictoris");
+        // nuevoPlaneta("LHS 1140 b", 8.3e24, 8800, "Copernico");
         
         // 2. Muestra por pantalla la lista de pasajeros de la cabina A-60-S
+        listaPCstmt = conn.prepareStatement("SELECT nombre, edad FROM pasajeros WHERE cubierta = ? AND numero_cabina = ? AND lado_cabina = ?");
+
+        listaPasajerosCabina("A", 60, "S");
+
         // 3. Muestra por pantalla una lista de sistemas, planetas y número de pasajeros con origen en ellos
         
 
@@ -46,8 +51,18 @@ public class Main {
     }
 
     private static void listaPasajerosCabina (String cubierta, int cabina, String lado) throws SQLException {
-        // @TODO Muestra por pantalla una lista de pasajeros de una cabina
-        
+        listaPCstmt.setString(1, cubierta);
+        listaPCstmt.setInt(2, cabina);
+        listaPCstmt.setString(3, lado);
+        ResultSet rs = listaPCstmt.executeQuery();
+
+        System.out.println("Pasajero(s) de la cabina " + cubierta + "-" + cabina + "-" + lado + ":");
+        while (rs.next()) {
+            String nombre = rs.getString("nombre");
+            int edad = rs.getInt("edad");
+
+            System.out.println("nombre = " + nombre + ", edad = " + edad);
+        }
     }
 
     private static void listaOrigenes() throws SQLException {
