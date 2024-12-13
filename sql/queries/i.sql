040bd45166d92b1d1fe6d58bb9feab6217f5939b
JhonyRetro <94227499+JhonyRetro@users.noreply.github.com>
2024-12-13 22:28:06 +0100

    refactor: test query i

--- sql/queries/i.sql
+++ sql/queries/i.sql
@@ -5,11 +5,11 @@
 SELECT cubierta, lado_cabina AS lado, numero_cabina AS numero
 FROM pasajeros p
          JOIN gastos g ON p.id = g.pasajero
-WHERE vip = 0 -- FIXME: im not sure this query is correct
+WHERE vip = 0 
 GROUP BY cubierta, lado_cabina, numero_cabina
 HAVING SUM(cantidad) > 2 * (SELECT AVG(total)
                             FROM (SELECT SUM(cantidad) AS total
                                   FROM pasajeros p
                                            JOIN gastos g ON p.id = g.pasajero
                                   GROUP BY cubierta, lado_cabina, numero_cabina) AS t1)
-   AND COUNT(id) > 3;
+   AND COUNT(p.id) > 3;
