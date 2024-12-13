762f368d7ed81b0f0ca8843bc194e0a538a7626c
JhonyRetro <94227499+JhonyRetro@users.noreply.github.com>
2024-12-13 21:27:36 +0100

    feat: add task n

---
+++ sql/triggers/n.sql
@@ -0,0 +1,29 @@
+#n ) Para evitar que pueda haber algún ciber-delincuente en alguno de los entrete-
+#nimientos ofrecidos por en la nave, se quieren crear tantos triggers como sean
+#necesarios para evitar que existan gastos en entretenimientos con saldo negativo,
+#es decir, la cantidad en gastos sea menor que 0. En caso de producirse el ciber-
+#ataque, deberá mostraserse el siguiente mensaje de error: “ERROR: Bienvenido
+#ciber-delincuente, pero no puedes sacar dinero en un entretenimiento”.
+DELIMITER //
+
+CREATE TRIGGER prevent_negative_expense_before_insert
+BEFORE INSERT ON gastos
+FOR EACH ROW
+BEGIN
+    IF NEW.cantidad < 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'ERROR: Bienvenido ciber-delincuente, pero no puedes sacar dinero en un entretenimiento';
+    END IF;
+END //
+
+CREATE TRIGGER prevent_negative_expense_before_update
+BEFORE UPDATE ON gastos
+FOR EACH ROW
+BEGIN
+    IF NEW.cantidad < 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'ERROR: Bienvenido ciber-delincuente, pero no puedes sacar dinero en un entretenimiento';
+    END IF;
+END //
+
+DELIMITER ;
