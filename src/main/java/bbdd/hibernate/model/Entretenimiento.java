6da535ef7abb1f1746c5c5d1e7e983b8ed01934e
JhonyRetro <94227499+JhonyRetro@users.noreply.github.com>
2024-12-15 20:10:57 +0100

    fix: fix table typo

--- src/main/java/bbdd/hibernate/model/Entretenimiento.java
+++ src/main/java/bbdd/hibernate/model/Entretenimiento.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "entretenimientos")
+@Table(name = "entretenimiento")
 public class Entretenimiento {
 
     @Id
--- src/main/java/bbdd/hibernate/model/Gasto.java
+++ src/main/java/bbdd/hibernate/model/Gasto.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 
 
 @Entity
-@Table(name = "gastos")
+@Table(name = "gasto")
 public class Gasto {
 
     @Id
