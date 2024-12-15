da5eff89c5a03d73081864b144591ee478f236b3
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-12-15 14:10:13 +0100

    fix(m): remake function

--- sql/functions/m.sql
+++ sql/functions/m.sql
@@ -1,34 +1,40 @@
-#m) Se quiere crear una función que permita obtener el porcentaje de pasajeros que
-#viajan a un destino concreto (sistema y planeta), para una cubierta determi-
-#nada, cuyos datos pasaremos como parámetro. A fin de comprobar su correcto
-#funcionamiento, crear una consulta que muestre la ocupación de todas las cu-
-#biertas disponibles para el destino “55 Cancri e” sistema “Copernico” ordenada
-#alfabéticamente por la letra de la cubierta.
-DELIMITER //
+# m) Se quiere crear una función que permita obtener el porcentaje de pasajeros que
+# viajan a un destino concreto (sistema y planeta), para una cubierta determinada,
+# cuyos datos pasaremos como parámetro. A fin de comprobar su correcto funcionamiento,
+# crear una consulta que muestre la ocupación de todas las cubiertas disponibles para
+# el destino “55 Cancri e” sistema “Copernico” ordenada alfabéticamente por la letra de
+# la cubierta.
 
-CREATE FUNCTION obtener_porcentaje_pasajeros(planeta_destino VARCHAR(255), sistema_destino VARCHAR(255), letra_cubierta VARCHAR(1))
-RETURNS DECIMAL(5,2)
-DETERMINISTIC
+DELIMITER //
+CREATE FUNCTION calcularPorcentajePasajeros(p_destino VARCHAR(50), s_destino VARCHAR(50), letra VARCHAR(1))
+    RETURNS DECIMAL(5, 2)
+    DETERMINISTIC
 BEGIN
     DECLARE total_pasajeros INT;
-    DECLARE pasajeros_cubierta INT;
-    DECLARE porcentaje DECIMAL(5,2);
+    DECLARE pasajeros_destino INT;
+    DECLARE res DECIMAL(5, 2);
 
-    SELECT COUNT(*) INTO total_pasajeros
+    SELECT COUNT(*)
+    INTO total_pasajeros
     FROM pasajeros
-    WHERE planeta_destino = planeta_destino AND sistema_destino = sistema_destino;
+    WHERE cubierta = letra;
 
-    SELECT COUNT(*) INTO pasajeros_cubierta
+    SELECT COUNT(*)
+    INTO pasajeros_destino
     FROM pasajeros
-    WHERE planeta_destino = planeta_destino AND sistema_destino = sistema_destino AND cubierta = letra_cubierta;
+    WHERE planeta_destino = p_destino
+      AND sistema_destino = s_destino
+      AND cubierta = letra;
 
-    SET porcentaje = (pasajeros_cubierta / total_pasajeros) * 100;
+    SET res = IF(total_pasajeros <> 0, (pasajeros_destino / total_pasajeros) * 100, 0);
 
-    RETURN porcentaje;
+    RETURN res;
 END //
+DELIMITER ;
 
-DELIMITER;
-
-SELECT letra, obtener_porcentaje_pasajeros('55 Cancri e', 'Copernico', letra) AS porcentaje_ocupacion
+SELECT letra, calcularPorcentajePasajeros('55 Cancri e', 'Copernico', letra) AS porcentaje_ocupacion
 FROM cubiertas
 ORDER BY letra;
+
+
+DROP FUNCTION calcularPorcentajePasajeros;
